b056d79cccff6964099647bb693b7fc8d28c8a45
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-05-30 15:17:31 +0700

    add horizontal scroll

--- App.js
+++ App.js
@@ -172,7 +172,50 @@ const App = () => {
               </View>
             </View>
           </View>
-          <View style={{ borderBottomColor: '#E8E9ED', borderBottomWidth: 1, marginBottom: 20, marginTop: 20}}></View>
+          <View style={{ borderBottomColor: '#E8E9ED', borderBottomWidth: 1, marginTop: 16}}></View>
+        </View>
+        {/* Nearby GO-FOOD */}
+        <View>
+          <View style={{height: 15, width: 60, marginLeft: 16}} >
+            <Image source={require('./logo/go-food.png')} style={{width: undefined, height: undefined, resizeMode: 'contain', flex: 1}}/>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 16, marginHorizontal: 16}}>
+            <Text style={{fontSize: 17, fontWeight: 'bold', color: '#1C1C1C'}}>Nearby Restaurant</Text>
+            <Text style={{fontSize: 17, fontWeight: 'bold', color: '#61A756'}}>See All</Text>
+          </View>
+          <ScrollView horizontal={true} style={{flexDirection: 'row', marginLeft: 16}}>
+            <View style={{marginRight: 16}}>
+              <View style={{height: 150, width: 150, borderRadius: 10}}>
+                <Image source={require('./dummy/go-food-kfc.jpg')} style={{width: undefined, height: undefined, resizeMode: 'cover', flex: 1, borderRadius: 4}}/>
+              </View>
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', marginTop: 12}}>KFC Aeon Mall</Text>
+            </View>
+            <View style={{marginRight: 16}}>
+              <View style={{height: 150, width: 150, borderRadius: 10}}>
+                <Image source={require('./dummy/go-food-gm.jpg')} style={{width: undefined, height: undefined, resizeMode: 'cover', flex: 1, borderRadius: 4}}/>
+              </View>
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', marginTop: 12}}>Bakmi GM Aeon Mall</Text>
+            </View>
+            <View style={{marginRight: 16}}>
+              <View style={{height: 150, width: 150, borderRadius: 10}}>
+                <Image source={require('./dummy/go-food-banka.jpg')} style={{width: undefined, height: undefined, resizeMode: 'cover', flex: 1, borderRadius: 4}}/>
+              </View>
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', marginTop: 12}}>Martabak Banka</Text>
+            </View>
+            <View style={{marginRight: 16}}>
+              <View style={{height: 150, width: 150, borderRadius: 10}}>
+                <Image source={require('./dummy/go-food-orins.jpg')} style={{width: undefined, height: undefined, resizeMode: 'cover', flex: 1, borderRadius: 4}}/>
+              </View>
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', marginTop: 12}}>Martabak Orins</Text>
+            </View>
+            <View style={{marginRight: 16}}>
+              <View style={{height: 150, width: 150, borderRadius: 10}}>
+                <Image source={require('./dummy/go-food-pak-boss.jpg')} style={{width: undefined, height: undefined, resizeMode: 'cover', flex: 1, borderRadius: 4}}/>
+              </View>
+              <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C', marginTop: 12}}>Ayam Bakar Pak Boss</Text>
+            </View>
+          </ScrollView>
+          <View style={{ borderBottomColor: '#E8E9ED', borderBottomWidth: 1, marginTop: 16, marginHorizontal: 16, marginBottom: 20}}></View>
         </View>
       </ScrollView>
       {/* bottom tab bar */}
